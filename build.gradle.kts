dc3ec938d41c847f365cda26d32f11ff8f766782
João Daniel <jotaf.daniel@gmail.com>
2025-06-08 10:18:46 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "org.aithana.platform.server"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("org.aithana.platform.server.MainKt")
+}
---
+++ src/main/kotlin/org/aithana/platform/server/Main.kt
@@ -0,0 +1,5 @@
+package org.aithana.platform.server
+
+fun main(args: Array<String>) {
+    println("running with ${args.joinToString(",")}...")
+}
