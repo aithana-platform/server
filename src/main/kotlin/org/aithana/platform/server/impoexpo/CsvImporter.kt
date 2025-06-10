3f324ca6fd6e05fba4d15450242350a419e49e22
João Daniel <jotaf.daniel@gmail.com>
2025-06-10 15:01:21 +0200

    refact: CsvImporter to use Csv definitions

--- src/main/kotlin/org/aithana/platform/server/impoexpo/CsvImporter.kt
+++ src/main/kotlin/org/aithana/platform/server/impoexpo/CsvImporter.kt
@@ -7,17 +7,6 @@ import java.io.Reader
 import java.lang.IndexOutOfBoundsException
 
 class CsvImporter: RawDataImporter {
-    companion object {
-        private const val CELL_DELIMITER = ";"
-        private const val N_HEADER_LINES = 1
-
-        private enum class IndexMapper(val index: Int) {
-            ID(0),
-            SECTION(1),
-            QUOTE(2)
-        }
-    }
-
     override fun import(reader: Reader): QuotesTable {
         val content = reader.readText()
 
@@ -25,16 +14,16 @@ class CsvImporter: RawDataImporter {
     }
 
     private fun splitCells(row: String): List<String> {
-        return row.split(CELL_DELIMITER).map { it.trim() }
+        return row.split(Csv.SEPARATOR).map { it.trim() }
     }
 
     private fun createRowProcessor(table: Table): (index: Int, row: List<String>) -> Unit {
         return { index: Int, row: List<String> ->
             try {
                 table.append(
-                    artifactId = row[IndexMapper.ID.index],
-                    section = row[IndexMapper.SECTION.index],
-                    quote = row[IndexMapper.QUOTE.index]
+                    artifactId = row[Csv.IndexMapper.ID.index],
+                    section = row[Csv.IndexMapper.SECTION.index],
+                    quote = row[Csv.IndexMapper.QUOTE.index]
                 )
             } catch (_: IndexOutOfBoundsException) {
                 val fixedIndex = index + 1
@@ -49,7 +38,7 @@ class CsvImporter: RawDataImporter {
         content
             .split("\n")
             .map(::splitCells)
-            .drop(N_HEADER_LINES)
+            .drop(Csv.N_HEADER_LINES)
             .forEachIndexed(this.createRowProcessor(table))
 
         return table
