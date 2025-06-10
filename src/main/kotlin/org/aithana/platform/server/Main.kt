34a16d283ba2520163d45fd9d17500235eba1bdf
João Daniel <jotaf.daniel@gmail.com>
2025-06-10 19:28:30 +0200

    expose Aithana as CLI

--- src/main/kotlin/org/aithana/platform/server/Main.kt
+++ src/main/kotlin/org/aithana/platform/server/Main.kt
@@ -1,5 +1,20 @@
 package org.aithana.platform.server
 
+import org.aithana.platform.server.core.AithanaBuilder
+
 fun main(args: Array<String>) {
-    println("running with ${args.joinToString(",")}...")
+    try {
+        val inputFileName = args[0]
+        val outputFileName = args[1]
+
+        AithanaBuilder()
+            .openEncodeUsingGemini()
+            .importFromCsv(inputFileName)
+            .exportToCsv(outputFileName)
+            .build()
+            .process()
+
+    } catch (ioobe: IndexOutOfBoundsException) {
+        throw RuntimeException("missing CLI parameter")
+    }
 }
