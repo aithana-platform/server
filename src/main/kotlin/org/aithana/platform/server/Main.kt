2c87a80d508e13e487a4abaef228bebe3e8d6fb1
João Daniel <jotaf.daniel@gmail.com>
2025-06-25 18:32:44 +0200

    fix aithana subcommands

--- src/main/kotlin/org/aithana/platform/server/Main.kt
+++ src/main/kotlin/org/aithana/platform/server/Main.kt
@@ -1,11 +1,6 @@
 package org.aithana.platform.server
 
 import com.github.ajalt.clikt.core.main
-import com.github.ajalt.clikt.core.subcommands
-import org.aithana.platform.server.presentation.cli.Aithana
-import org.aithana.platform.server.presentation.cli.aithana.BatchCsvInOut
-import org.aithana.platform.server.presentation.cli.aithana.CoderCallCount
+import org.aithana.platform.server.presentation.cli.Aithana as AithanaCLI
 
-fun main(args: Array<String>) = Aithana()
-    .subcommands(BatchCsvInOut(), CoderCallCount())
-    .main(args)
+fun main(args: Array<String>) = AithanaCLI().main(args)
--- src/main/kotlin/org/aithana/platform/server/presentation/cli/Aithana.kt
+++ src/main/kotlin/org/aithana/platform/server/presentation/cli/Aithana.kt
@@ -1,8 +1,16 @@
 package org.aithana.platform.server.presentation.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import org.aithana.platform.server.presentation.cli.aithana.BatchCsvInOut
+import org.aithana.platform.server.presentation.cli.aithana.CoderCallCount
 
 class Aithana: CliktCommand() {
+
+    init {
+        subcommands(BatchCsvInOut(), CoderCallCount())
+    }
+
     override fun run() {
         echo("Running Aithana from CLI")
     }
